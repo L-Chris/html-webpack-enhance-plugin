4b6c515d5e41b2c49fcbc1308df640fb5ce49730
L-Chris <563303226@qq.com>
2018-09-28 13:42:42 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # html-webpack-enhance-plugin
+enhance html-webpack-plugin features
+
+## Install
+```
+npm i html-webpack-enhance-plugin -D
+yarn add html-webpack-enhance-plugin -D
+```
+
+## ✨ Features:
+
+- override publicPath options in webpack config
+
+## Usage
+
+**webpack.config.js**
+
+```
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackEnhancePlugin = require('html-webpack-enhance-plugin')
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new HtmlWebpackEnhancePlugin({
+      publicPath: ''
+    })
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = class HtmlWebpackEnhancePlugin {
+  constructor (options) {
+    this.options = options
+  }
+
+  apply (compiler) {
+    const self = this
+
+    compiler.hooks.compilation.tap('HtmlWebpackEnhance', function (compilation) {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('HtmlWebpackEnhance', function (data, callback) {
+        const oldPublicPath = self.getOriginPublicPath(compilation)
+        const {assets} = data
+
+        // deal with assets
+        Object.keys(assets).forEach(key => {
+          const assetList = assets[key]
+          if (!Array.isArray(assetList)) return
+          assetList.forEach((_, i) => {
+            if (!assets[key][i].path) return
+            assets[key][i].path = assets[key][i].path.replace(new RegExp(`${oldPublicPath}`), self.options.publicPath)
+          })
+        })
+
+        // deal with html
+        data.html = data.html.replace(new RegExp(`="${oldPublicPath}`, 'g'), `="${self.options.publicPath}`)
+
+        callback(null, data)
+      })
+
+    })
+  }
+
+  getOriginPublicPath (compilation) {
+    const compilationHash = compilation.hash
+
+    let publicPath = typeof compilation.options.output.publicPath !== 'undefined'
+      // If a hard coded public path exists use it
+      ? compilation.mainTemplate.getPublicPath({hash: compilationHash})
+      // If no public path was set get a relative url path
+      : path.relative(path.resolve(compilation.options.output.path, path.dirname(this.childCompilationOutputName)), compilation.options.output.path)
+        .split(path.sep).join('/');
+
+    if (publicPath.length && publicPath.substr(-1, 1) !== '/') {
+      publicPath += '/';
+    }
+
+    return publicPath
+  }
+}
