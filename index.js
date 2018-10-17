63b3d04e3930f225ea9f3c4c2bcf258067116e60
L-Chris <563303226@qq.com>
2018-10-17 17:05:42 +0800

    fix: webpack3

--- index.js
+++ index.js
@@ -5,45 +5,12 @@ module.exports = class HtmlWebpackEnhancePlugin {
 
   apply (compiler) {
     const self = this
-
-    compiler.hooks.compilation.tap('HtmlWebpackEnhance', function (compilation) {
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('HtmlWebpackEnhance', function (data, callback) {
-        const oldPublicPath = self.getOriginPublicPath(compilation)
-        const {assets} = data
-
-        // deal with assets
-        Object.keys(assets).forEach(key => {
-          const assetList = assets[key]
-          if (!Array.isArray(assetList)) return
-          assetList.forEach((_, i) => {
-            if (!assets[key][i].path) return
-            assets[key][i].path = assets[key][i].path.replace(new RegExp(`${oldPublicPath}`), self.options.publicPath)
-          })
-        })
-
-        // deal with html
-        data.html = data.html.replace(new RegExp(`="${oldPublicPath}`, 'g'), `="${self.options.publicPath}`)
-
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin('html-webpack-plugin-after-html-processing', function (data, callback) {
+        const oldPublicPath = data.assets.publicPath
+        data.html = data.html.replace(new RegExp(`=(")?${oldPublicPath}`, 'g'), `=$1${self.options.publicPath}`)
         callback(null, data)
       })
-
     })
-  }
-
-  getOriginPublicPath (compilation) {
-    const compilationHash = compilation.hash
-
-    let publicPath = typeof compilation.options.output.publicPath !== 'undefined'
-      // If a hard coded public path exists use it
-      ? compilation.mainTemplate.getPublicPath({hash: compilationHash})
-      // If no public path was set get a relative url path
-      : path.relative(path.resolve(compilation.options.output.path, path.dirname(this.childCompilationOutputName)), compilation.options.output.path)
-        .split(path.sep).join('/');
-
-    if (publicPath.length && publicPath.substr(-1, 1) !== '/') {
-      publicPath += '/';
-    }
-
-    return publicPath
   }
 }
